7129abd1572236d20959171855242daf4a4fd336
Luis Albuquerque <lmiguelaa@gmail.com>
2018-04-29 12:49:10 +0100

    preparar para podermos usar tabelas de Hash

--- SQLtoLADSL.h
+++ SQLtoLADSL.h
@@ -1,6 +1,17 @@
- #include <stdio.h>                                                                                                                      
- #include <strings.h>
+#include <stdio.h>                                                                                                                      
+#include <strings.h>
+#include <glib.h>
 
+/* -----GLIB----- */
+#define insertDecs(...) g_hash_table_insert(hDecs,__VA_ARGS__)
+#define lookupDecs(...) g_hash_table_lookup(hDecs,__VA_ARGS__)
+#define removeDecs(...) g_hash_table_remove(hDecs,__VA_ARGS__)
+
+#define insertFuns(...) g_hash_table_insert(hFuns,__VA_ARGS__)
+#define lookupFuns(...) g_hash_table_lookup(hFuns,__VA_ARGS__)
+#define removeFuns(...) g_hash_table_remove(hFuns,__VA_ARGS__)
+
+/* -------------- */
 
 typedef struct node{
   char* string;
@@ -21,10 +32,29 @@ FILE* out;
 int flag = 0;
 char* tables;
 
+typedef struct hashData{
+    char type;
+    int pt;
+    int len;
+    GSList *dim;
+} *HashData;
+
+/* -----declarations.c----- */
 void add_rename(char* rename, Select select);
 
 void add_Literalfilter(char* filter,char* table, Select select);
 
 void add_filter(char* atributo1, char* atributo2, char* op, char* table, Select select);
 
-void join (int flag, char* atributo1, char* atributo2);
+//void join (int flag, char* atributo1, char* atributo2);
+
+HashData newHashData(char type, int pt, GSList *dim);
+
+int converToIndex(GString *name, GSList *indexL);
+
+int calcLen(GSList *dim);
+
+void hdPrint(HashData hd);
+
+
+/* ------------------------ */
--- declarations.c
+++ declarations.c
@@ -21,6 +21,62 @@ void add_filter(char* atributo1, char* atributo2, char* op, char* table, Select 
   printf
 }
 
+int calcLen(GSList *dim){
+    int len = 1;
+    if(dim == NULL) return 0;
+    for(GSList *it = dim; it; it=it->next){
+        len *= (int)it->data;
+    }
+
+    return len;
+}
+
+HashData newHashData(char type, int pt, GSList *dim){
+    int len = calcLen(dim);
+    HashData hd = (HashData)malloc(sizeof(struct hashData));
+    hd->type = type;
+    hd->pt = pt;
+    hd->len = len;
+    hd->dim = dim;
+    return hd;
+}
+
+int converToIndex(GString *name, GSList *indexL){
+    HashData hd = lookupDecs(name->str);
+    int ret = 0;
+    int i=0;
+
+    for (GSList *itIndex = indexL; itIndex; itIndex = itIndex->next){
+        int par = (int)itIndex->data;
+        int j=0;
+        for(GSList *itDim = hd->dim; itDim && j<i; itDim = itDim->next){
+            par *= (int)itDim->data;
+            j++;
+        }
+        i++;
+        ret += par;
+    }
+    return ret;
+}
+
+int calcLen(GSList *dim){
+    int len = 1;
+    if(dim == NULL) return 0;
+    for(GSList *it = dim; it; it=it->next){
+        len *= (int)it->data;
+    }
+
+    return len;
+}
+
+void hdPrint(HashData hd){
+    printf("estrutura a inserir na hash\n");
+    printf("\ttype: %c\n",hd->type);
+    printf("\tpointer: %d\n",hd->pt);
+    printf("\tlength: %d\n",hd->len);
+    printf("\n");
+}
+
 /*
 void join (int flag, char* atributo1, char* atributo2){
     switch(flag){
