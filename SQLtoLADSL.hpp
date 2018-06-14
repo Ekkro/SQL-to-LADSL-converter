cda046e41f2a84c6238864e4a6fbb12ca2119ab6
Luis Albuquerque <lmiguelaa@gmail.com>
2018-06-14 11:51:52 +0100

    corrigir erros

--- SQLtoLADSL.hpp
+++ SQLtoLADSL.hpp
@@ -4,14 +4,140 @@
 #include <map>
 #include <utility>
 using namespace std;
+class Graph {
+    string root;
+    map<string, pair<string,string> > join;
+    vector<pair <string,string> > groupby;
+    map<string,map<string,int> > tables; //int-> 0=Measure, 1 = Dimension
+    map<string, vector<string> > filter;
+    vector<string> select;
+        public:
+
+        int num_attributes(string str){
+            return tables[str].size();
+        }
+
+        void add_filter(string Table,string filter, int type = -1){
+            tables[Table].insert(pair<string,int>(filter,type));
+        }
+
+        void remove_filter(string Table,string filter){
+            tables[Table].erase(filter);
+        }
+
+        int search_filter_in_table(string table, string filter){
+            for(map<string,int>::iterator it = tables[table].begin(); it != tables[table].end(); ++it) {
+                if (it->first.compare(filter)==0) {return 1;}
+            }
+            return 0;
+        }
+
+        int search_table(string table){
+            for(map<string, map<string,int> >::iterator it = tables.begin(); it != tables.end(); ++it) {
+                if (it->first.compare(table)==0) {return 1;}
+            }
+            return 0;
+        }
+
+        void add_table(string Table, string filter, int type){
+            map<string,int> aux;
+            aux[filter] = type;
+            pair<map<string, int>::iterator, bool > result;
+            tables.insert(pair<string,map<string,int> >(Table, aux));
+        }
+
+        void remove_table(string Table){
+            tables.erase(Table);
+        }
+
+        // join[Table] é um par
+//        void add_join(string Table,string filter, string type ){
+//            pair<map<string, string>::iterator, bool > result;
+//            join[Table].insert(pair<string,string>(filter,type));
+//        }
+
+        // join[Table] é um par
+//        void remove_join(string Table,string filter){
+//            join[Table].erase(filter);
+//        }
+
+        void add_groupby(string Table,string filter){
+            groupby.push_back(pair<string,string>(Table,filter));
+        }
+
+        //groupby é um vetor, groupby[i] é um par 
+        void remove_groupby(string Table,string filter){
+            for (int i = 0; i < groupby.size(); i++) {
+                if ((groupby[i].first.compare(Table)==0) && (groupby[i].second.compare(filter)==0)) {
+                    groupby.erase(groupby.begin() + i);
+                    break;
+                }
+            }
+        }
+
+        void add_map_filter(string Table, string Filter){
+            //filter[Table].push_back(filter);
+            filter[ Table ].push_back(Filter);
+        }
+
+        void remove_map_Table_filter(string Table){
+            filter.erase(Table);
+        }
+
+        void remove_map_filter(string Table,string Filter){
+            for (int i = 0; i < filter[Table].size(); i++) {
+                if (filter[Table][i].compare(Filter)==0){
+                    filter[Table].erase(filter[Table].begin() + i);
+                    break;
+                }
+            }
+        }
+
+        void add_select(string Table){
+            select.push_back(Table);
+        }
+
+        void remove_select(string Table){
+            for (int i = 0; i < select.size(); i++) {
+                if (select[i].compare(Table)==0) {
+                    select.erase(select.begin() + i);
+                    break;
+                }
+            }
+        }
+
+//        int is_measured(string filter){
+//            if (attribute_type[filter] == 1) {
+//                return 1;
+//            }else return 0;
+//        }
+//
+//        int is_dimension(string filter){
+//            if (attribute_type[filter] == 2) {
+//                return 1;
+//            }else return 0;
+//        }
+
+//        void add_table(string Table, string name){
+//            Graph[Table].insert(filter,str);
+//        }
+//
+//        int isThe_same_Table(string attribute1, string attribute2){
+//            if (!attribute_table[attribute1].compare(attribute_table[attribute2])) {
+//                return 1;
+//            }
+//            else return 0;
+//         }
+};
+
 class Ltree {
 
-    vector<String> ltree;
-    //vector<pair<String,String>> ltree; possivelmente será necessário alterar o codigo para esta ltree
-    Public:
+    vector<string> ltree;
+    //vector<pair<string,string>> ltree; possivelmente será necessário alterar o codigo para esta ltree
+    public:
 //        Ltree() {ltree = new vector<string>();}
-        String Parent(int indice){
-            String res = "NULL";
+        string Parent(int indice){
+            string res = "NULL";
             int aux = (indice-1)/2;
             if ( aux <0 || aux >= ltree.size()) {
                 return res;
@@ -25,12 +151,17 @@ class Ltree {
             }else{ return aux; }
             }
 
-        String left_child(int indice){
-            String res = "NULL";
+        string left_child(int indice){
+            string res = "NULL";
             int aux = 2*indice+1;
             if ( aux <0 || aux >= ltree.size()) {
                 return res;
             }else{ return ltree[aux]; }
+            }
+
+        vector<string>::iterator it_left_child(vector<string>::iterator indice){
+            indice = indice+1;
+            return indice; 
             }
 
         int ind_left_child(int indice){
@@ -40,8 +171,8 @@ class Ltree {
             }else{ return aux; }
             }
 
-        String right_child(int indice){
-            String res = "NULL";
+        string right_child(int indice){
+            string res = "NULL";
             int aux = 2*indice+2;
             if ( aux <0 || aux >= ltree.size()) {
                 return res;
@@ -55,9 +186,9 @@ class Ltree {
             }else{ return aux; }
             }
 
-        void add(String value ,int indice ){ ltree[indice] = value; }
+        void add(string value ,int indice ){ ltree[indice] = value; }
 
-        int indice(String value){
+        int indice(string value){
            for ( int i = 0; i < ltree.size(); i++ ) {
                if (!ltree[i].compare(value)) {
                    return i;
@@ -67,7 +198,7 @@ class Ltree {
         }
 
         void swap(int indice1,int indice2 ){
-            String aux;
+            string aux;
             aux = ltree[indice1];
             ltree[indice1] = ltree[indice2];
             ltree[indice2] = aux;
@@ -75,51 +206,55 @@ class Ltree {
 
         vector<int> parents(int i){
            vector<int> res;
+           vector<int>::iterator it;
+           it = res.begin();
            for (int ind = i;ind_Parent(i)>=0;i = ind_Parent(i)) {
-               res.insert(i);
+               //res.insert(i);
+               it = res.insert (it , i);
            }
            return res;
         }
 
-        String common_ancestor( int indice1, int indice2 ){
+        string common_ancestor( int indice1, int indice2 ){
             vector<int> parent_1 = parents(indice1);
             vector<int> parent_2 = parents(indice2);
             for ( int i = 0;i < parent_1.size(); i++) {
-              for ( int j = 0; j < parent_2.size();j++)){}
+              for ( int j = 0; j < parent_2.size();j++)
                 if (parent_1[i] == parent_2[j]) {
                     return(ltree[parent_1[i]]);
                 }
             }
+            return "NULL";
         }
 
-        void merge(vector<String> vec){
+        void merge(vector<string> vec){
             for ( int i = 0; ltree.size(); i++) {
-                if ((!ltree[i].compare("NULL")) && (vec.compare("NULL")==0)) {
-                    ltree[i] = vec[i]
+                if ((!ltree[i].compare("NULL")) && (vec[i].compare("NULL")==0)) {
+                    ltree[i] = vec[i];
                 }
             }
         }
 
-        vector<String> split_aux(vector<String> res, int indice1, int indice2){
+        vector<string> split_aux(vector<string> res, int indice1, int indice2){
             if (!ltree[indice2].compare("NULL")) {
                 return res;
             }
             res[indice1] = ltree[indice2];
-            vector<String> aux1 = ltree.split_aux(res,ind_left_child(indice1),ind_left_child(indice2));
-            vector<String> aux2 = ltree.split_aux(res,ind_right_child(indice1),ind_right_child(indice2));
-            return aux1.merge(aux2);
-
+            vector<string> aux1 = Ltree::split_aux(res,ind_left_child(indice1),ind_left_child(indice2));
+            vector<string> aux2 = Ltree::split_aux(res,ind_right_child(indice1),ind_right_child(indice2));
+            //return aux1.merge(aux2);
+            return res;
         }
 
-        vector<String> split(int indice1, int indice2){
-            vector<String> res;
-            return  ltree.split_aux(res,indice1,indice2);
+        vector<string> split(int indice1, int indice2){
+            vector<string> res;
+            return  Ltree::split_aux(res,indice1,indice2);
         }
 
-        void add_tree(vector<String> v,int ind, int n=0){
+        void add_tree(vector<string> v, vector<string>::iterator ind, int n=0){
                 if (v[n] != "NULL") {
-                    ltree.add(v[n],ind);
-                    add_tree(v,ind_left_child(ind),ind_left_child(n));
+                    ltree.insert(ind,v[n]);
+                    add_tree(v,it_left_child(ind),it_left_child(n));
                     add_tree(v,ind_right_child(ind),ind_right_child(n));
                 }
         }
@@ -152,7 +287,7 @@ class Ltree {
         void funcao(Graph g, int x=0){
             if(ltree.ltree[x] == "OR" && g.isThe_same_Table(ltree.left_child(x),ltree.right_child(x)){
                 int parent = ltree.ind_Parent(x);
-                    vector<String> s = ltree.split(0,parent);
+                    vector<string> s = ltree.split(0,parent);
                     ltree.add_tree(s,ltree.ind_left_child(x));
                     ltree.add_tree(s,ltree.ind_right_child(x));
                     ltree.add("OR",parent);
@@ -164,131 +299,10 @@ class Ltree {
             }
           }
     }
+};
 
 
-}
-
-class Graph {
-    String root;
-    Map<String, pair<String,String> > join;
-    vector<pair <String,String> > groupby;
-    Map<String,Map<String,int> > tables; //int-> 0=Measure, 1 = Dimension
-    Map<String, vector<String> > filter;
-    vector<String> select;
-        Public:
-
-        int num_attributes(String str){
-            return tables[str].size();
-        }
-
-        void add_filter(String Table,String filter, int type = -1){
-            tables[Table].insert(pair<String,int>(filter,type));
-        }
-
-        void remove_filter(String Table,String filter){
-            tables[Table].erase(filter);
-        }
-
-        int search_filter_in_table(String table, String filter){
-            for(map<String,int>::iterator it = tables[Table].begin(); it != tables[Table].end(); ++it) {
-                if (it->first.compare(filter)==0) {return 1;}
-            }
-            return 0;
-        }
-
-        int search_table(String table){
-            for(map<String,Map<String,int>>::iterator it = tables.begin(); it != tables.end(); ++it) {
-                if (it->frist.compare(Table)==0) {return 1;}
-            }
-            return 0;
-        }
-
-        void add_table(String Table, String filter, int type){
-            map<String,int> aux;
-            aux[filter] = type;
-            tables.insert(pair<String,Map<String,int>(Table, aux));
-        }
-
-        void remove_table(String Table){
-            tables.erase(Table);
-        }
-
-        void add_join(String Table,String filter, String type ){
-            join[Table].insert(pair<String,String>(filter,type));
-        }
-
-        void remove_join(String Table,String filter){
-            join[Table].erase(filter);
-        }
-
-        void add_groupby(String Table,String filter){
-            groupby.push_back(pair<String,String>(Table,filter));
-        }
-
-        void remove_groupby(String Table,String filter){
-            for (int i = 0; i < groupby.size(); i++) {
-                if ((groupby[i].getKey().compare(Table)==0) && (groupby[i].getValue().compare(filter)==0)) {
-                    groupby.erase(groupby.begin() + i);
-                    break();
-                }
-            }
-        }
-
-        void add_map_filter(String Table, String filter){
-            filter[Table].push_back(filter);
-        }
-
-        void remove_map_Table_filter(String Table){
-            filter.erase(Table);
-        }
-
-        void remove_map_filter(String Table,String filter){
-            for (int i = 0; i < filter[Table].size(); i++) {
-                if (filter[Table][i].compare(Table)==0){
-                    filter[Table].erase(filter[Table].begin() + i);
-                    break();
-                }
-            }
-        }
-
-        void add_select(String Table){
-            select.push_back(Table);
-        }
-
-        void remove_select(String Table){
-            for (int i = 0; i < select.size(); i++) {
-                if (select[i].compare(Table)==0) {
-                    select.erase(select.begin() + i);
-                    break();
-                }
-            }
-        }
-
-        int is_measured(String filter){
-            if (attribute_type[filter] == 1) {
-                return 1;
-            }else return 0;
-        }
-
-        int is_dimension(String filter){
-            if (attribute_type[filter] == 2) {
-                return 1;
-            }else return 0;
-        }
-
-        void add_table(String Table, String name){
-            graph[Table].insert(filter,str);
-        }
-
-        int isThe_same_Table(String attribute1, String attribute2){
-            if (!attribute_table[attribute1].compare(attribute_table[attribute2])) {
-                return 1;
-            }
-            else return 0;
-
- }
-
-String a = "A";
+string a = "A";
 Graph g;
 LTree l;
 
@@ -303,7 +317,7 @@ void graphWork(){
 
 void graphWorkAux(int x){
   //pega nas pontas
-  String start = giveMeStart(g.root);
+  string start = giveMeStart(g.root);
   //juntar filtros do mesmo atributo dessa tabela, no final a tabela terá aoenas letras.
   joinAtr(start);
   //juntar tudo
@@ -312,14 +326,14 @@ void graphWorkAux(int x){
   if(x == 1)removeTable(start);
 }
 
-void joinAtr(String start){
+void joinAtr(string start){
   //pega nos filtros no mesmo atributo
-  Map<String,int> work = g.tables[start];
-  vetor<String> v;
-  vector<String,int> new_elements;
-  for(Map<String,int>::iterator it = work.begin(); it != work.end(); ++it) {
-    String s = it->first;
-    vector<String> v = g.filter[s];
+  map<string,int> work = g.tables[start];
+  vetor<string> v;
+  vector<string,int> new_elements;
+  for(map<string,int>::iterator it = work.begin(); it != work.end(); ++it) {
+    string s = it->first;
+    vector<string> v = g.filter[s];
     if(it->secound == 0){ //se for medida
       joinAtrM(s,v);
     }else{                //se for dimensao
@@ -330,9 +344,9 @@ void joinAtr(String start){
   g.tables[start] = new_elements;
 }
 
-void joinAtrM(String s, vector<String> v){
-  vector<String> resolvidos;
-  vector<String> relacoes;
+void joinAtrM(string s, vector<string> v){
+  vector<string> resolvidos;
+  vector<string> relacoes;
   resolvidos.append(v[0]);
   for(x = 1; x < v.lenght() < v; x++){
     relacoes.append(relacao_entre(resolvidos,v[x]));
@@ -353,9 +367,9 @@ void joinAtrM(String s, vector<String> v){
   }
 }
 
-void joinAtrD(String s, vector<String> v){
-  vector<String> resolvidos;
-  vector<String> relacoes;
+void joinAtrD(string s, vector<string> v){
+  vector<string> resolvidos;
+  vector<string> relacoes;
   resolvidos.append(v[0]);
   for(x = 1; x < v.lenght() < v; x++){
     relacoes.append(relacao_entre(resolvidos,v[x],s));
@@ -363,7 +377,7 @@ void joinAtrD(String s, vector<String> v){
   next();
   cout << a << "=filter("<< s << v[0] << ")\n";
   for(x = 1; x < resolvidos.lenght(); x++){ // letra = AND/OR(filter(),letra_anterior);
-    String aux = a;
+    string aux = a;
     next();
     cout << a << "=";
     if(relacoes[x-1].compare("AND")){
@@ -375,13 +389,13 @@ void joinAtrD(String s, vector<String> v){
   }
 }
 
-void joinFilters(String start){
+void joinFilters(string start){
   //pega nos filtros da mesma tabela
-  Map<String,int> work = g.tables[start];
-  vector<String> medidas;
-  vector<String> dimensoes;
-  for(Map<String,int>::iterator it = work.begin(); it != work.end(); ++it) {
-    String s = it->first;
+  map<string,int> work = g.tables[start];
+  vector<string> medidas;
+  vector<string> dimensoes;
+  for(map<string,int>::iterator it = work.begin(); it != work.end(); ++it) {
+    string s = it->first;
     if(it->secound == 0){ //se for medida
       medidas.append(s);
     }else{                //se for dimensao
@@ -389,52 +403,52 @@ void joinFilters(String start){
     }
   }
   if( medidas.size() == 1){
-    String aux = medias[0];
+    string aux = medias[0];
   }else{
     joinFiltersM(medidas);
-    String aux = a;
+    string aux = a;
   }
   if( dimensoes.size() == 1){
-    String aux2 = medias[0];
+    string aux2 = medias[0];
   }else{
     joinFiltersD(dimensoes);
     if(dimensoes.size()>0 && medidas.size()>0){
-    String aux2 = a;
+    string aux2 = a;
   }
     next();
     cout << a << "=" << "krao(" << aux << "," << aux2 << ")\n";
   }
 }
 
-void joinFiltersM(vector<String> v){
+void joinFiltersM(vector<string> v){
   next();
   cout << a << "= hadamard(" << v[0] << "," << v[1] << ")\n";
   for(int x = 1; x < v.lenght() ; x++){
-    String aux = a;
+    string aux = a;
     next();
     cout << a << "=" << "hadamard(" << aux << "," << v[x] << ")\n";
   }
 }
 
-void joinFiltersD(vector<String> v){
+void joinFiltersD(vector<string> v){
   next();
   cout << a << "= krao(" << v[0] << "," << v[1] << ")\n";
   for(int x = 1; x < v.lenght() ; x++){
-    String aux = a;
+    string aux = a;
     next();
     cout << a << "=" << "krao(" << aux << "," << v[x] << ")\n";
   }
 }
 
 
-void removeTable(String start){
-  vector<String> removedKeys;
-  String alpha = a;
-  for(Map<String,pair<String,String>>::iterator it = g.join.begin(); it != g.join.end(); ++it) {
+void removeTable(string start){
+  vector<string> removedKeys;
+  string alpha = a;
+  for(map<string,pair<string,string>>::iterator it = g.join.begin(); it != g.join.end(); ++it) {
     if(it->secound->secound.compare(start)){
       next();
       cout << a << "=" << "dot(" << it->first << "," << alpha << ")\n";
-      Map<String,int> aux = g.tables[start];
+      map<string,int> aux = g.tables[start];
       aux[a] = 1;           //possivel zona a melhorar
       g.tables[start] = aux;
       removedKeys.append(it->first);
@@ -447,9 +461,9 @@ void removeTable(String start){
 
 
 
-String giveMeStart(String root){
-  vector<String> values;
-  for(Map<String,pair<String,String>>::iterator it = g.join.begin(); it != g.join.end(); ++it) {
+string giveMeStart(string root){
+  vector<string> values;
+  for(map<string,pair<string,string>>::iterator it = g.join.begin(); it != g.join.end(); ++it) {
     values.push_back(it->secound);
   }
   for(x = 0; x < values.lenght(); x++){
